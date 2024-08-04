5b49d95321d8b626bc504f2a0ceda8cd25b7374e
nagarajuv <nagarajuv@jfrog.com>
2024-08-04 16:53:31 +0530

    updated

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const app = express();
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://nagaraju:nagaraju@cluster0.vbdphob.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 
 app.use(bodyParser.json());
@@ -23,6 +25,29 @@ mongoose.connect(process.env.MONGO_URI, {
       //  console.log("MongoDB Connection Successful")
     }
 })
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 var Schema = mongoose.Schema;
 
